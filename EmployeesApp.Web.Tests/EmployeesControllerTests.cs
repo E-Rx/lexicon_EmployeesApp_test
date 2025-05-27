93a38ed6f1b3fddf56361f48e96781e8d03d91cd
Laagren <simonagren@hotmail.se>
2025-05-27 14:24:04 +0200

    added tests to employeesControllerTests

--- EmployeesApp.Web.Tests/EmployeesControllerTests.cs
+++ EmployeesApp.Web.Tests/EmployeesControllerTests.cs
@@ -4,6 +4,8 @@ using Moq;
 using EmployeesApp.Application.Employees.Interfaces;
 using EmployeesApp.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using EmployeesApp.Web.Views.Employees;
+using System.ComponentModel.DataAnnotations;
 namespace EmployeesApp.Web.Tests;
 
 public class EmployeesControllerTests
@@ -13,15 +15,10 @@ public class EmployeesControllerTests
     [Fact]
 
     public void Index_NoParams_ReturnsViewResult()
-
     {
-
         var Service = new Mock<IEmployeeService>();
-
         Service
-
             .Setup(o => o.GetAll())
-
             .Returns([
 
                 new Employee { Id = 1, Name = "Test company 1", Email = "London@www.se" },
@@ -40,5 +37,52 @@ public class EmployeesControllerTests
 
     }
 
+    [Fact]
+    public void Create_HttpGet_ReturnsViewResult()
+    {
+        var service = new Mock<IEmployeeService>();
+        var controller = new EmployeesController(service.Object);
+        var result = controller.Create();
+        Assert.IsType<ViewResult>(result);
+    }
+
+    [Fact]
+    public void Create_HttpPost_ReturnsViewResult()
+    {
+        //var service = new Mock<IEmployeeService>();
+        //service
+        //    .Setup(o => o.GetAll())
+        //    .Returns([
+        //        new Employee { Id = 1, Name="Testname", Email="test@email.com"}
+        //        ]);
+        //var controller = new EmployeesController (service.Object);
+        //var result = controller.Create();
+        var model = new CreateVM
+        {
+            Name = "Test",
+            Email = "test@email.com",
+            BotCheck = 4
+        };
+
+        var context = new ValidationContext(model);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(model, context, results, validateAllProperties: true);
+        Assert.True(isValid);
+    }
+
+
+    [Fact]
+    public void Details_ReturnsViewResult()
+    {
+        var service = new Mock<IEmployeeService>();
+        service
+            .Setup(o => o.GetById(1))
+            .Returns( new Employee { Id = 1, Name="Testname", Email="test@email.com"});
+
+        var controller = new EmployeesController(service.Object);
+        var result = controller.Details(1);
+        Assert.IsType<ViewResult>(result);
+        
+    }
 }
  
