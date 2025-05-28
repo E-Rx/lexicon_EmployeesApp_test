2b16dbe1826d511c3ae474ccc9b796828cd9a8b4
E-Rx <elise.rochaix@gmail.com>
2025-05-28 09:07:40 +0200

    controller test changes

--- EmployeesApp.Web.Tests/EmployeesControllerTests.cs
+++ EmployeesApp.Web.Tests/EmployeesControllerTests.cs
@@ -21,11 +21,12 @@ public class EmployeesControllerTests
             .Setup(o => o.GetAll())
             .Returns([
 
-                new Employee { Id = 1, Name = "Test company 1", Email = "London@www.se" },
+                new Employee { Id = 1, Name = "Employee1", Email = "employee1@www.se" },
 
-                new Employee { Id = 2, Name = "Test company 2", Email = "London@www.se" },
+                new Employee { Id = 2, Name = "Employee2", Email = "employee2@www.se" },
 
-                new Employee { Id = 3, Name = "Test company 3", Email = "Malmö@www.se" }
+                new Employee { Id = 3, Name = "Employee3", Email = "employee3@www.se" }
+
 
                 ]);
 
@@ -82,6 +83,7 @@ public class EmployeesControllerTests
         var controller = new EmployeesController(service.Object);
         var result = controller.Details(1);
         Assert.IsType<ViewResult>(result);
+        
         
     }
 }
